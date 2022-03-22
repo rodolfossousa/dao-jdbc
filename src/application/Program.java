1364505644d64295153de39800352a29dd6def8d
rodolfossousa <rock.sousa@gmail.com>
2022-03-22 18:30:49 -0300

    Factory Dao

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.VendedorDao;
 import model.entities.Departamento;
 import model.entities.Vendedor;
 
@@ -14,6 +16,8 @@ public class Program {
 		System.out.println(obj);
 		
 		Vendedor vendedor  = new Vendedor(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		
+		VendedorDao vendedorDao = DaoFactory.createVendedorDao();
 		
 		System.out.println(vendedor);
 
---
+++ src/model/dao/DaoFactory.java
@@ -0,0 +1,10 @@
+package model.dao;
+
+import model.dao.impl.VendedorDaoJDBC;
+
+public class DaoFactory {
+
+	public static VendedorDao createVendedorDao() {
+		return new VendedorDaoJDBC();
+	}
+}
