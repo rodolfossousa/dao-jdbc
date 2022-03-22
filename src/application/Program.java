a71b2049fa4d9176df379158dd479d73620d46dc
rodolfossousa <rock.sousa@gmail.com>
2022-03-22 18:17:27 -0300

    Classe Vendedor

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Departamento;
+import model.entities.Vendedor;
 
 public class Program {
 
@@ -9,6 +12,10 @@ public class Program {
 		Departamento obj = new Departamento(1, "Livros");
 		
 		System.out.println(obj);
+		
+		Vendedor vendedor  = new Vendedor(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		
+		System.out.println(vendedor);
 
 	}
 
