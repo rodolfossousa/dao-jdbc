7410de47b674381d514931224a2863aa1a9c8943
rodolfossousa <rock.sousa@gmail.com>
2022-03-23 22:04:46 -0300

    FindByDepartamento implementado

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.VendedorDao;
+import model.entities.Departamento;
 import model.entities.Vendedor;
 
 public class Program {
@@ -14,7 +17,14 @@ public class Program {
 		Vendedor vendedor = vendedorDao.fyndById(3);
 		
 		System.out.println(vendedor);
-
+		
+		System.out.println("\n=== TEST 2: Vendedor findByDepartamento ===");
+		Departamento dep = new Departamento(2, null);
+		List<Vendedor> vendedores = vendedorDao.findByDepartamento(dep);
+		
+		for(Vendedor obj : vendedores) {
+			System.out.println(obj);
+		}
 	}
 
 }
--- src/model/dao/VendedorDao.java
+++ src/model/dao/VendedorDao.java
@@ -2,6 +2,7 @@ package model.dao;
 
 import java.util.List;
 
+import model.entities.Departamento;
 import model.entities.Vendedor;
 
 public interface VendedorDao {
@@ -11,4 +12,5 @@ public interface VendedorDao {
 	void deleteById(Integer id);
 	Vendedor fyndById(Integer id);
 	List<Vendedor> findAll();
+	List<Vendedor> findByDepartamento(Departamento departamento);
 }
